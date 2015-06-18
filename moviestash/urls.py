ba21712db33c30f9d4fbbbefc6d137918863221d
Daniel Ansong <D.ansong00@gmail.com>
2015-06-18 07:26:59 -0400

    no message

--- moviestash/urls.py
+++ moviestash/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, patterns
 from rest_framework.urlpatterns import format_suffix_patterns
-from . import views
+from moviestash import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.MovieList.as_view()),
